5a54a63cebd6092d85adffd12a6ca4d4ae9ca418
MalineeSunrise <malineebukong@gmail.com>
2026-02-27 16:25:55 +0700

    Week7

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,24 +8,45 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     private InputAction jumpAction;
 
+    private bool isOnGround;
+    public bool isGameOver = false;
+
     void Awake()
     {
+        isGameOver = false;
         rb = GetComponent<Rigidbody>();
         jumpAction = InputSystem.actions.FindAction("Jump");
+        
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        Physics.gravity *= gravityMultiplier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (jumpAction.triggered)
+        if (jumpAction.triggered && isOnGround == true && isGameOver == false)
         {
-
+            rb.AddForce(jumpForce * Vector3.up , ForceMode.Impulse);
+            isOnGround = false;
         }
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Debug.Log("hit" + collision.gameObject.name);
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("GAME OVER");
+            isGameOver = true;
+        }
+    }
+
 }
